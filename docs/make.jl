cabe2ed0572c4625e6a59c196ace24d095a4a862
Francesco Alemanno <50984334+francescoalemanno@users.noreply.github.com>
2020-06-18 17:17:47 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,15 +3,13 @@ using Documenter, ApproxInferenceBase
 makedocs(
     modules = [ApproxInferenceBase],
     format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
-    authors = "Johanni Brea",
+    authors = "Julia Approx Inference members",
     sitename = "ApproxInferenceBase.jl",
     pages = Any["index.md"]
     # strict = true,
     # clean = true,
     # checkdocs = :exports,
 )
-
 deploydocs(
     repo = "github.com/JuliaApproxInference/ApproxInferenceBase.jl.git",
-    push_preview = true
 )
