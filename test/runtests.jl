aa28564ce5ba1e4f9b8a06e8f4b5307c2a3290a1
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-06-18 21:56:04 +0200

    add tests for mixed value support

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,4 +11,10 @@ using Random
     @test logpdf(d,(0.5,100.5)) == 0.0
     @test logpdf(d,(0.0,0.0)) == -Inf
     @test length(d) == 2
+    m=Factored(Uniform(0.00,1.0),DiscreteUniform(1,2))
+    sample=rand(m)
+    @test 0<sample[1]<1
+    @test sample[2] == 1 || sample[2] == 2
+    @test pdf(m,sample) == 0.5
+    @test logpdf(m,sample) ≈ log(0.5)
 end
