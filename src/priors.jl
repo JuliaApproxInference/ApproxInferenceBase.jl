b378efd63de321469b60f32e8a76b43348de01e5
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-06-18 21:12:00 +0200

    specialization for faster code

--- src/priors.jl
+++ src/priors.jl
@@ -18,28 +18,39 @@ end
 
 Function to evaluate the pdf of a `Factored` distribution object
 """
-pdf(d::Factored,x) = prod(i->pdf(d.p[i],x[i]),eachindex(x))
+function pdf(d::Factored{N},x) where N
+    s=pdf(d.p[1],x[1])
+    for i in 2:N
+        s*=pdf(d.p[i],x[i])
+    end
+    s
+end
 
 """
     logpdf(d::Factored, x) = begin
 
 Function to evaluate the logpdf of a `Factored` distribution object
 """
-logpdf(d::Factored,x) = sum(i->logpdf(d.p[i],x[i]),eachindex(x))
-
+function logpdf(d::Factored{N},x) where N
+    s=logpdf(d.p[1],x[1])
+    for i in 2:N
+        s+=logpdf(d.p[i],x[i])
+    end
+    s
+end
 
 """
     rand(rng::AbstractRNG, factoreddist::Factored)
 
 function to sample one element from a `Factored` object
 """
-rand(rng::AbstractRNG,factoreddist::Factored) = rand.(Ref(rng),factoreddist.p)
+rand(rng::AbstractRNG,factoreddist::Factored{N}) where N = ntuple(i->rand(rng,factoreddist.p[i]),Val(N))
 
 """
     length(p::Factored) = begin
 
 returns the number of distributions contained in `p`.
 """
-length(p::Factored) = sum(length.(p.p))
+length(p::Factored{N}) where N = N
 
 export Factored
