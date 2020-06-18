78015ade994a520bad9675495b663f4104f5ec8c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-06-18 15:09:39 +0000

    build based on a0daadd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ApproxInferenceBase-1","page":"ApproxInferenceBase","title":"ApproxInferenceBase","text":"","category":"section"},{"location":"#","page":"ApproxInferenceBase","title":"ApproxInferenceBase","text":"Documentation goes here.","category":"page"}]
+}
